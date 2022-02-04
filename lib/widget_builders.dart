7246dd2c9a57e1fc80dd646ed9b60b1086e49d32
iOS <ios@MacOss-iMac-Pro-4.local>
2022-02-04 09:04:24 +0530

    remove unused import

--- lib/widget_builders.dart
+++ lib/widget_builders.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_ui_builder/get_device_type.dart';
 import 'package:responsive_ui_builder/sizing_information.dart';
